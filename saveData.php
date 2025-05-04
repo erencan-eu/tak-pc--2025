39c2dc7c74dab641d9ef3dfb288715e13af72bcc
erencan-eu <yunusuyar1989@gmail.com>
2025-05-04 03:19:45 +0300

    Update saveData.php

--- saveData.php
+++ saveData.php
@@ -21,3 +21,41 @@
     </form>
 </body>
 </html>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Formdan gelen verileri al
+    $adana = $_POST['adana'];
+    $sehir = $_POST['sehir'];
+    $kutucuk = $_POST['kutucuk'];
+
+    // Verileri dizi olarak kaydet
+    $data = [
+        'adana' => $adana,
+        'sehir' => $sehir,
+        'kutucuk' => $kutucuk,
+        'tarih' => date('Y-m-d H:i:s')  // Zaman bilgisini ekleyelim
+    ];
+
+    // Veriyi JSON formatında dosyaya kaydet
+    $jsonFile = 'veriler.json';  // JSON dosyasının adı
+
+    // Eğer dosya varsa, veriyi mevcut dosyaya ekle
+    if (file_exists($jsonFile)) {
+        $existingData = json_decode(file_get_contents($jsonFile), true);
+    } else {
+        $existingData = [];
+    }
+
+    // Yeni veriyi mevcut veriye ekle
+    $existingData[] = $data;
+
+    // Güncellenmiş veriyi JSON formatında kaydet
+    file_put_contents($jsonFile, json_encode($existingData, JSON_PRETTY_PRINT));
+
+    // Başarı mesajı
+    echo "Veri başarıyla kaydedildi!";
+} else {
+    echo "Lütfen formu doğru şekilde doldurun.";
+}
+?>
+
